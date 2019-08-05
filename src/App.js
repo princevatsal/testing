a117b495f9d427b385ec1322908602d105c1e287
Priyansh Vatsal <priyanshvatsal@gmail.com>
2019-08-05 09:57:47 -0700

    'firstt'

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import React ,{Component} from 'react'
+
+const defaultStyle={
+  color:'#fff'
+}
+class Aggregate extends Component{
+  render(){
+    return(
+      <div style={{width:'50%',display:'inline-block'}}>
+        <h2 style={defaultStyle}>Number Text</h2>
+      </div>
+    )
+  }
+}
+class Filter extends Component{
+  render(){
+    return(
+      <div style={{...defaultStyle,textAlign:'center',margin:'20px 5px'}}>
+        <img/>
+        <input type="text"/>
+        Filter
+      </div>
+      )
+  }
+}
+
+class Playlist extends Component{
+render(){
+  return(
+  <div style={{color:'white',width:'25%',display:'inline-block'}}>
+    <h3>playlist Name</h3>
+    <ul>
+      <li>Song 1</li>
+      <li>Song 2</li>
+      <li>Song 3</li>
+    </ul>
+  </div>
+  )
+}
+}
+
+class main extends Component{
+  render(){
+    return(
+      <div style={{padding:'5px 70px'}}>
+      <h1 style={{textAlign:'center',fontSize:'40px',color:'white'}}>Title</h1>
+      <Aggregate/>
+      <Aggregate/>
+      <Filter/>
+      <Playlist/>
+      <Playlist/>
+      <Playlist/>
+      <Playlist/>
+      </div>
+      )
+  }
+}
+
+export default main
