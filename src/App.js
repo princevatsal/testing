d3b6de9f91ef303ab378e81ca70197022dab5760
Priyansh Vatsal <priyanshvatsal@gmail.com>
2019-08-07 01:41:04 -0700

    fetching from spotify 2

--- src/App.js
+++ src/App.js
@@ -117,7 +117,7 @@ class main extends Component{
         {playlisttorender.map(playlist=><Playlist playlist={playlist}/>)}
       </div>:
         <button onClick={()=>
-          {window.location='http://localhost:8888/login'}} 
+          {window.location= window.location.href.includes('localhost')?'http://localhost:8888/login':'https://my-playlist.herokuapp.com/login'}} 
           style={
             {border:'none',
             fontSize:'20px',
